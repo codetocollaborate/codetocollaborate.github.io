63bacdde232b2cf2ad58adf230702b15eb7468fd
Apeksha Rahangdale <75223083+codetocollaborate@users.noreply.github.com>
2022-01-22 15:13:16 +0100

    Update skills.js

--- data/skills/skills.js
+++ data/skills/skills.js
@@ -2,22 +2,22 @@ export default [
 	{
 		skillName: "JavaScript, C++",
 		color: "1",
-	    percentage: "70"
+	    percentage: "80"
     },
     {
 		skillName: "Angular, Reactjs",
 		color: "6",
-	    percentage: "50"
+	    percentage: "80"
+	},
+    {
+		skillName: "Python",
+		color: "6",
+	    percentage: "70"
 	},
     {
 		skillName: "Nodejs, MongoDB",
 		color: "2",
-	    percentage: "40"
-	},
-    {
-		skillName: "AWS(EC2, S3), Heroku, Netlify",
-		color: "3",
-	    percentage: "30"
+	    percentage: "70"
 	},
     {
 		skillName: "HTML, Bootstrap, SCSS",
@@ -25,7 +25,7 @@ export default [
 	    percentage: "70"
 	},
     {
-		skillName: "GitHub, JIRA, Trello",
+		skillName: "GitHub, JIRA, Jenkins, Google Analytics",
 		color: "7",
 	    percentage: "70"
 	},
@@ -35,13 +35,13 @@ export default [
 	    percentage: "80"
 	},
     {
-		skillName: "CleverTap, FreshDesk",
+		skillName: "AdobeXD, CreativeCloud, Figma",
 		color: "5",
-	    percentage: "50"
+	    percentage: "20"
 	},
     {
 		skillName: "VS Code",
 		color: "6",
-	    percentage: "70"
+	    percentage: "80"
 	}
 ];
