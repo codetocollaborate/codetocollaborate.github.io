025ab224e359fbaba058c90663037a28454c0c19
Apeksha Rahangdale <75223083+codetocollaborate@users.noreply.github.com>
2022-01-22 17:12:21 +0100

    Create userResearch.js

---
+++ data/projects/userResearch.js
@@ -0,0 +1,10 @@
+export default [
+      {
+        projectName: "Human Robot Interaction",
+        image: "images/programmingdiaries.png",
+        summary:
+          "Psychological study",
+        preview: "https://codetocollaborate.github.io/",
+        techStack: ["Choreographe"],
+      }
+  ];
