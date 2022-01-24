dbfbad34684ab5b4fd72b6d83e5c84edc00d361c
Apeksha Rahangdale <75223083+codetocollaborate@users.noreply.github.com>
2022-01-24 21:07:54 +0100

    Update designs.js

--- data/projects/designs.js
+++ data/projects/designs.js
@@ -1,1 +1,18 @@
-
+export default [
+	{
+		projectName: "Fortune",
+		image: "images/fortune.png",
+		summary:
+			"Wheel of fortune you can turn for yourself",
+		preview: "https://github.com/codetocollaborate",
+		techStack: ["Creative cloud"],
+	},
+  {
+		projectName: "Meditate",
+		image: "images/meditate.png",
+		summary:
+			"Meditate to come back to present and align future",
+		preview: "https://github.com/codetocollaborate",
+		techStack: ["Creative cloud"],
+	}
+];
